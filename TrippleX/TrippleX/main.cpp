49c607c06d7073c38d15035f1b1eb76d1dc9a207
Jacquelynne <jacheima@uat.edu>
2021-01-25 12:54:16 -0800

    Finished the Game
    I added two new fuctions. The play game function takes a difficulty argument and makes the game harder based on the difficulty level. The game gets more difficult the more levels you complete. I also added a helper function that handles printing the welcome message for each level. This also takes the difficulty parameter and uses it to print out which level the player is on.
    
    The game uses rand() to pick a random range. The range is computed to be the difficulty + the difficulty.
    
    Then the player has to guess what numbers the computer generated. If they get it right they move on to the next level, increasing the difficulty. If not, they have to retry the level.
    
    Once they hit level 10 the game ends.

--- TrippleX/TrippleX/main.cpp
+++ TrippleX/TrippleX/main.cpp
@@ -1,24 +1,83 @@
 #include <iostream>
+#include <ctime>
 
 using std::cout;
+using std::cin;
+
+void PrintIntroduction(int Difficulty)
+{
+	cout << "--------------------------------------------\n";
+	cout << "You're a secret agent breaking into a LEVEL " << Difficulty << " server room...\n";
+	cout << "You need to enter the correct code to continue...\n\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+	PrintIntroduction(Difficulty);
+
+	//declare 3 number code
+	int CodeDigitA = rand() % Difficulty + Difficulty;
+	int CodeDigitB = rand() % Difficulty + Difficulty;
+	int CodeDigitC = rand() % Difficulty + Difficulty;
+
+	const int CodeSum = CodeDigitA + CodeDigitB + CodeDigitC;		//14
+	const int CodeProduct = CodeDigitA * CodeDigitB * CodeDigitC;	//64
+
+	//give the player some hints
+	cout << "+ There are 3 numbers in the code.\n";
+	cout << "+ The numbers add up to be: " << CodeSum << "\n";
+	cout << "+ The numbers multiply to be: " << CodeProduct << "\n";
+
+	//store player guesses
+	int GuessA, GuessB, GuessC;
+
+	cout << "Enter three numbers followed by an x: ";
+
+	//get player guesses
+	cin >> GuessA >> GuessB >> GuessC;
+
+	int GuessSum = GuessA + GuessB + GuessC;
+	int GuessProduct = GuessA * GuessB * GuessC;
+
+	//check if the player made the right guess
+	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+	{
+
+		cout << "\n*** Well done agent. You've extracted a file! Keep going! ***\n\n";
+		return true;
+	}
+	else
+	{
+
+		cout << "\n*** You're code didn't work. Try again, agent. ***\n\n";
+		return false;
+	}
+}
 
 int main()
 {
-	//Print welcom message to the terminal
-	cout << "You're a secret agent breaking into a secure server room...\n";
-	cout << "You need to enter the correct code to continue...\n";
+	srand(time(NULL));
 
-	//declare 3 number code
-	const int a = 4;
-	const int b = 8;
-	const int c = 2;
+	const int MaxDifficulty = 10;
+	int LevelDifficulty = 1;
 
-	const int sum = a + b + c;		//14
-	const int product = a * b * c;	//64
+	char playAgain = 'n';
 
-	//print sum and product to the terminal
-	cout << "The sum of the all the numbers is equal to " << sum << ".\n";
-	cout << "The product of the all the numbers is " << product << ".\n";
+	while (LevelDifficulty <= MaxDifficulty)
+	{
+		bool bCompletedLevel = PlayGame(LevelDifficulty);
+
+		cin.clear();
+		cin.ignore();
+
+		if (bCompletedLevel)
+		{
+			LevelDifficulty++;
+		}
+	}
+
+	cout << "Great work agent! You got all the files! Now get out of there!\n";
 
 	return 0;
 }
+
